bb8434fd2e89cbe205e090f2183d98abb0a8054c
Ahmet Yardımcı <ahmet.yardimci@devbase.com.tr>
2021-08-31 13:16:45 +0300

    Add project files.

---
+++ PdfClassification/Options.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PdfClassification
+{
+    public class Options
+    {
+        public string SourcePath { get; set; }
+        public string DestinationPath { get; set; }
+        public LogicalOperation LogicalOperation { get; set; }
+        public TransferType TransferType { get; set; }
+        public List<string> Keywords { get; set; }
+    }
+    public enum LogicalOperation
+    {
+        Or = 0,
+        And = 1
+
+    }
+    public enum TransferType
+    {
+        Copy = 1,
+        Move = 2
+    }
+
+}
---
+++ PdfClassification/Program.cs
@@ -0,0 +1,79 @@
+﻿using BitMiracle.Docotic.Pdf;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PdfClassification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Options options = ConfigurationBuilder();
+
+            var files = Directory.GetFiles(options.SourcePath, "*.pdf");
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string filePath = files.GetValue(i).ToString();
+
+                Classification(filePath, options.DestinationPath, options.LogicalOperation, options.TransferType, options.Keywords);
+            }
+
+        }
+        static bool IsExist(string filePath, LogicalOperation logicalOperation, List<string> keywords)
+        {
+            bool result = false;
+
+            PdfDocument pdf = new PdfDocument(filePath);
+
+            for (int i = 0; i < pdf.Pages.Count; i++)
+            {
+                string pageText = pdf.Pages[i].GetText();
+
+                foreach (var item in keywords)
+                {
+                    int index = pageText.IndexOf(item, 0, StringComparison.OrdinalIgnoreCase);
+
+                    if (logicalOperation == LogicalOperation.Or) //  veya ||
+                    {
+                        if (index != -1) result = true;
+                    }
+
+                    if (logicalOperation == LogicalOperation.And) // ve && 
+                    {
+                        if (index == -1) result = false;
+                    }
+                }
+
+            }
+            pdf.Dispose();
+            return result;
+        }
+        static void Classification(string filePath, string destinationPath, LogicalOperation logicalOperation, TransferType transferType, List<string> keywords)
+        {
+
+            var fileName = Path.GetFileName(filePath);
+            if (IsExist(filePath, logicalOperation, keywords))
+                FileTransfer(fileName, $"{destinationPath}{fileName}", transferType);
+        }
+
+        static void FileTransfer(string sourceFile, string destinationFile, TransferType transferType)
+        {
+            if (transferType == TransferType.Move)
+                File.Move(sourceFile, destinationFile);
+            if (transferType == TransferType.Copy)
+                File.Copy(sourceFile, destinationFile);
+
+        }
+        private static Options ConfigurationBuilder()
+        {
+            var builder = new ConfigurationBuilder()
+                         .AddJsonFile($"appsettings.json", true, true);
+            var config = builder.Build();
+            var options = config.Get<Options>();
+            return options;
+        }
+    }
+}
